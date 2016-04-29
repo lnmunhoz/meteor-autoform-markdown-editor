9a0654f762809d377f6d2de792f4b9ff5fda3d88
lnmunhoz <ln.munhoz@gmail.com>
2016-04-29 16:41:15 -0300

    Update package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lnmunhoz:autoform-markdown-editor',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'A github like markdown editor using autoform and perak:markdown',
   git: 'https://github.com/lnmunhoz/meteor-autoform-markdown-editor',
   documentation: 'README.md'
